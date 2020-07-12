60eb8af4c3d70cfce643b6860b429a9c33cdd348
Adarsh Kumar <2017csb1064@iitrpr.ac.in>
2020-07-12 13:05:13 +0530

    Enable App routes

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+// Import all the components here so that you may route amongst them
+// Header and Footer components are not needed because we are not navigating in Header and Footer
+
+// Template: import { 'ComponentClassName' } from '../component_name/component_name.component.ts'
+import { HomeComponent } from '../home/home.component';
+import { AuthComponent } from '../auth/auth.component';
+
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: AuthComponent},
+];
