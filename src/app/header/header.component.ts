235d7300c42792aeef857e835d8b603e6df162fd
Adarsh Kumar <adkumar@chegg.com>
2021-04-02 18:01:01 +0530

    Fix automatic loading of user name

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/models';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  currentUser: User;
   title = 'Course Registration Portal';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(authService: AuthService) {
+    authService.currentUser.subscribe((user) => (this.currentUser = user));
   }
 
-  googleSignIn(){
+  ngOnInit(): void {}
+
+  googleSignIn() {
     console.log('Google Login function called!!!');
   }
 }
