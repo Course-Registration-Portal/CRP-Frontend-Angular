05ca4442004054037bc59596cac6f8b4efaf3b72
Adarsh Kumar <adkumar@chegg.com>
2021-04-03 14:47:36 +0530

    Move authorization to header component from auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { SocialAuthService, SocialUser } from 'angularx-social-login';
-import { GoogleLoginProvider } from 'angularx-social-login';
-
-import { AuthService } from './auth.service';
 
 import { BASEURL } from '../shared/baseURL';
 
@@ -13,36 +9,13 @@ import { BASEURL } from '../shared/baseURL';
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-  user: SocialUser;
-  loggedIn: boolean;
   selectedCSV: File;
 
-  constructor(
-    private socialAuthService: SocialAuthService,
-    private http: HttpClient,
-    private authService: AuthService
-  ) {
+  constructor(private http: HttpClient) {
     this.selectedCSV = null;
-    this.socialAuthService.authState.subscribe((user) => {
-      if (user) {
-        this.user = user;
-        this.authService.login(user.authToken);
-        this.loggedIn = user != null;
-      }
-    });
   }
 
-  signInWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-
-  signOut(): void {
-    this.authService.logout();
-  }
-
-  ngOnInit(): void {
-    this.loggedIn = this.authService.isAuthenticated();
-  }
+  ngOnInit(): void {}
 
   onFileSelected(event) {
     this.selectedCSV = event.target.files[0];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 import { User } from '../auth/models';
 
 @Component({
@@ -9,13 +11,35 @@ import { User } from '../auth/models';
 })
 export class HeaderComponent implements OnInit {
   currentUser: User;
+  loggedIn: boolean;
+
   title = 'Course Registration Portal';
 
-  constructor(authService: AuthService) {
+  constructor(
+    private socialAuthService: SocialAuthService,
+    private authService: AuthService
+  ) {
     authService.currentUser.subscribe((user) => (this.currentUser = user));
+
+    this.socialAuthService.authState.subscribe((user) => {
+      if (user) {
+        this.authService.login(user.authToken);
+        this.loggedIn = user != null;
+      }
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loggedIn = this.authService.isAuthenticated();
+  }
+
+  signInWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.logout();
+  }
 
   googleSignIn() {
     console.log('Google Login function called!!!');
