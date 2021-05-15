40e8a42a7946a550263290b9f9847199c4ada1f5
Adarsh Kumar <adkumar@chegg.com>
2021-05-15 17:25:58 +0530

    Create modal for User & contain the user role in it

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+export class User {
+  pk: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  token: string;
+  role: string;
+
+  constructor(user) {
+    this.pk = user.pk;
+    this.username = user.username;
+    this.email = user.email;
+    this.first_name = user.first_name;
+    this.last_name = user.last_name;
+    this.token = user.token;
+    this.role = user.role;
+  }
+}
